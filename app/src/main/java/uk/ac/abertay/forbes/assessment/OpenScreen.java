e377a03dada3294f5628b5a77f82584adee05e66
yeroc_sebrof <1500812@abertay.ac.uk>
2018-03-16 20:45:57 +0000

    Got tired and did a bunch of the theme and a bit of back end

--- app/src/main/java/uk/ac/abertay/forbes/assessment/OpenScreen.java
+++ app/src/main/java/uk/ac/abertay/forbes/assessment/OpenScreen.java
@@ -1,6 +1,10 @@
 package uk.ac.abertay.forbes.assessment;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,17 +12,24 @@ import android.view.View;
 public class OpenScreen extends AppCompatActivity {
 
     @Override
+    protected void onDestroy() {
+        //String = SQLiteDatabase.execSQL("DROP DATABASE logs");
+        super.onDestroy();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /*
-        try {
+        // Poke the database table to life or to wake it up
+        //SQLiteDatabase.openOrCreateDatabase("Logs", null).close();
+
+        /* try {
             setContentView(R.layout.activity_open_screen);
             wait(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        */
+        } */
 
         setContentView(R.layout.activity_main_menu);
     }
