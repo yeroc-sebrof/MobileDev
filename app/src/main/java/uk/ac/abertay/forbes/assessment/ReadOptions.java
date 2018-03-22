8673593a4e59808ded78ea64a1a2075f165cc0ef
yeroc-sebrof <1500812@abertay.ac.uk>
2018-03-22 18:27:55 +0000

    Added the delete toggle. Need to toast to the user that its been activated

--- app/src/main/java/uk/ac/abertay/forbes/assessment/ReadOptions.java
+++ app/src/main/java/uk/ac/abertay/forbes/assessment/ReadOptions.java
@@ -2,9 +2,13 @@ package uk.ac.abertay.forbes.assessment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,25 +18,48 @@ import java.util.List;
  */
 
 public class ReadOptions extends Activity {
+    boolean deleting = Boolean.FALSE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_options);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
+        final ListView lv = findViewById(R.id.list_logs);
 
-        ListView lv = findViewById(R.id.list_logs);
+        // Check for a database containing a list of the databases.
 
-        // if (!databaseResponse) {
+        // if (there is not a databases containing a log) {
         dummyPopulate(lv);
         // }
+        // else { populate the list with the logs that exist }
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str = (String)(Object) lv.getItemAtPosition(position);
+                if (deleting)
+                    { Toast.makeText(getApplicationContext(),
+                        "Deleting: " + str,
+                        Toast.LENGTH_SHORT).show();
+                        // delete current item clicked
+                        //check if this updates the list upon completetion or if the page must refresh
+                    }
+                else
+                    { Toast.makeText(getApplicationContext(),
+                        str,
+                        Toast.LENGTH_SHORT).show();
+                        openLog(view, str);
+                    }
+            }
+        });
     }
 
     public void dummyPopulate(ListView lv) {
         List<String> dummyValues = new ArrayList<String>();
 
-        dummyValues.add("No Responses, A list");
-        dummyValues.add("would look like this tho");
+        dummyValues.add("No Responses");
+        dummyValues.add("List item Spam");
 
         for (int x = 1; x <= 10; x++) dummyValues.add("Item " + x);
 
@@ -40,6 +67,15 @@ public class ReadOptions extends Activity {
                 (this, android.R.layout.simple_list_item_1, dummyValues);
 
         lv.setAdapter(arrayAdapter);
+    }
+
+    public void delLog(View view) {
+        if (deleting == Boolean.TRUE) {deleting = Boolean.FALSE;} else {deleting = Boolean.TRUE;}
+    }
+
+    public void openLog(View view, String str) {
+        Intent intent = new Intent(this, ReadingLog.class);
+        startActivity(intent);
     }
 
 }
