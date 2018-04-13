152094c1992c28d66a7906ade0dbbef57b96c705
yeroc-sebrof <1500812@uad.ac.uk>
2018-04-13 19:05:14 +0100

    Classes inside main class and correctly concurrent readlog(s)

--- app/src/main/java/uk/ac/abertay/forbes/assessment/asyncDatabaseHelper.java
+++ app/src/main/java/uk/ac/abertay/forbes/assessment/asyncDatabaseHelper.java
@@ -14,19 +14,19 @@ public class asyncDatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     // Handler Database Name
-    public static final String DATABASE_NAME = "logs_db";
+    private static final String DATABASE_NAME = "logs_db";
 
     // Handler Table
     // Handler Table Name
-    public static final String HANDLER_TABLE_NAME = "logs";
+    private static final String HANDLER_TABLE_NAME = "logs";
 
     // Handle Table Columns
     // Tables will be created == LOG + id
-    public static final String COLUMN_ID = "id";
-    public static final String LOG = "log";
-    public static final String COLUMN_TIMESTAMP = "timestamp";
-    public static final String COLUMN_TYPE = "type";
-    public static final String COLUMN_CONTENT = "content";
+    private static final String COLUMN_ID = "id";
+    private static final String LOG = "log";
+    private static final String COLUMN_TIMESTAMP = "timestamp";
+    private static final String COLUMN_TYPE = "type";
+    private static final String COLUMN_CONTENT = "content";
 
     // So the storage in the db just has to be a int it will be pulled and the type will be TYPES[<db int>]
     public final String[] TYPES = {"GPS", "Phone Call", "SMS"};
@@ -35,11 +35,19 @@ public class asyncDatabaseHelper extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //Handler handler;// = new Handler();
+
+    private ReadLogs readLogsActivity = null;
+    private ReadingLog readingLogActivity = null;
+
     // Create Tables
     @Override
-    public void onCreate(SQLiteDatabase db) {
-    asyncOnCreate asyncTask = new asyncOnCreate(db);
-    asyncTask.execute();
+        public void onCreate(SQLiteDatabase db) {
+        asyncOnCreate asyncTask = new asyncOnCreate(db);
+        asyncTask.execute();
+
+       // handler.dispatchMessage();
+
     }
 
     // Upgrade method to improve usability
@@ -51,15 +59,23 @@ public class asyncDatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public Cursor readLog(SQLiteDatabase db, int id) {
+    public void readLog(SQLiteDatabase db, int id, ReadingLog parent) {
         asyncReadLog asyncTask = new asyncReadLog(db);
-        return asyncTask.doInBackground(id);
+        readingLogActivity = parent;
+        asyncTask.execute(id);
     }
 
-    public Cursor readLogs(SQLiteDatabase db) {
+    public void readLogs(SQLiteDatabase db, ReadLogs parent) {
         // This is now Significantly slower than without async. Will continue to async all tasks tho
         asyncReadLogs asyncTask = new asyncReadLogs(db);
-        return asyncTask.doInBackground();
+        readLogsActivity = parent;
+        asyncTask.execute();
+    }
+
+    public void resetLogs (SQLiteDatabase db) {
+        asyncResetLogs asyncTask = new asyncResetLogs(db);
+
+        asyncTask.execute();
     }
 
     public Cursor lastItemInLogIndex (SQLiteDatabase db) {
@@ -87,178 +103,209 @@ public class asyncDatabaseHelper extends SQLiteOpenHelper {
 
     public String getDatabaseName()
     { return DATABASE_NAME; }
+
+    class asyncOnUpgrade extends AsyncTask<Void, Void, Void> {
+        private SQLiteDatabase db;
+        asyncOnUpgrade(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "ReadLogs task created");
+            db = database;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Make sure there's not a old table to interupt this process
+            db.execSQL("DROP TABLE IF EXISTS " + asyncDatabaseHelper.HANDLER_TABLE_NAME + "_OLD;");
+
+            // Rename table
+            db.execSQL("ALTER TABLE " + asyncDatabaseHelper.HANDLER_TABLE_NAME +
+                    " RENAME TO " + asyncDatabaseHelper.HANDLER_TABLE_NAME + "_OLD;");
+
+            // Transfer Data from old table
+            // TODO when relevant
+
+            return null;
+        }
+    }
+
+    class readIndexLastLog extends AsyncTask<Void, Void, Cursor> {
+        private SQLiteDatabase db;
+        readIndexLastLog(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "ReadLogs task created");
+            db = database;
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return db.query(HANDLER_TABLE_NAME, new String[]{COLUMN_ID},
+                    null, null, null, null,
+                    COLUMN_ID + " DESC", "1");
+        }
+    }
+
+    class asyncReadLog extends AsyncTask<Integer, Void, Cursor> {
+        private SQLiteDatabase db;
+        asyncReadLog(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "ReadLog task created");
+            db = database;
+        }
+
+        @Override
+        protected Cursor doInBackground(Integer... ints) {
+            return db.query(LOG + ints[0],
+                    new String[] {COLUMN_TYPE, COLUMN_CONTENT, COLUMN_TIMESTAMP},
+                    null, null, null, null,
+                    COLUMN_ID + " ASC",
+                    null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            readingLogActivity.startList(cursor);
+        }
+    }
+
+    class asyncReadLogs extends AsyncTask<Void, Void, Cursor> {
+        private SQLiteDatabase db;
+
+        asyncReadLogs(SQLiteDatabase database) {
+            Log.d("Database Helper", "ReadLogs task created");
+            db = database;
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return db.query(HANDLER_TABLE_NAME,
+                    new String[]{COLUMN_ID, LOG, COLUMN_TIMESTAMP},
+                    null, null, null, null,
+                    COLUMN_ID + " ASC",
+                    null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            readLogsActivity.startList(cursor);
+        }
+    }
+
+    class asyncResetLogs extends AsyncTask<Void, Void, Cursor> {
+        private SQLiteDatabase db;
+        asyncResetLogs(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "ReadLogs task created");
+            db = database;
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            return db.query(HANDLER_TABLE_NAME, new String[]{COLUMN_ID, LOG, COLUMN_TIMESTAMP},
+                    null, null, null, null,
+                    COLUMN_ID + " ASC",
+                    null);
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            readLogsActivity.logs = cursor;
+            readLogsActivity.logs.moveToFirst();
+        }
+    }
+
+
+    class asyncOnCreate extends AsyncTask<Void, Void, Void> {
+        private SQLiteDatabase db;
+        asyncOnCreate(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "On create task created");
+            db = database;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String CREATE_HANDLER_TABLE =
+                    "CREATE TABLE IF NOT EXISTS " + HANDLER_TABLE_NAME
+                            + "("
+                            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            + LOG + " TINYTEXT,"
+                            + COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+                            + ");";
+
+            db.execSQL(CREATE_HANDLER_TABLE);
+            return null;
+        }
+    }
+
+    class asyncDelLog extends AsyncTask<Integer, Void, Void> {
+        private SQLiteDatabase db;
+        asyncDelLog(SQLiteDatabase database)
+        {
+            Log.d("Database Helper", "Delete Log created");
+            db = database;
+        }
+
+        @Override
+        protected Void doInBackground(Integer... ints) {
+            String del = "DROP TABLE IF EXISTS " + LOG + ints[0] + ";";
+            db.execSQL(del);
+            Log.d("Database Helper","Dropped Table Log " + ints[0]);
+
+            db.delete(HANDLER_TABLE_NAME,
+                    COLUMN_ID + "=" + ints[0],
+                    null);
+            Log.d("Database Helper","Removed Index ID " + ints[0]);
+
+            return null;
+        }
+    }
+
+    class asyncNewLog extends AsyncTask<Void, Void, Void> {
+        private SQLiteDatabase db;
+        private ContentValues insertValue = new ContentValues(0);
+
+        asyncNewLog(SQLiteDatabase database, String name)
+        {
+            Log.d("Database Helper", "Make new log created");
+            db = database;
+
+            // For the sake of readability would prefer none to be null
+            if (name.equals("")) { name = null; }
+            insertValue.put(LOG, name);
+            Log.d("Database Helper", "Value Inserted to Index");
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Log.d("Database Helper", "Make new log called");
+            db.insert(HANDLER_TABLE_NAME,
+                    null,
+                    insertValue);
+            Log.d("Database Helper", "Value Inserted to Index");
+
+            // Query to get the new Log ID to work with
+            final Cursor logNo = db.query(HANDLER_TABLE_NAME,
+                    new String[]{COLUMN_ID},
+                    null, null, null, null,
+                    COLUMN_ID + " DESC", "1");
+            logNo.moveToFirst();
+
+            // Make a new log table
+            String CREATE_LOG = "CREATE TABLE IF NOT EXISTS " + LOG + logNo.getString(0)
+                    + "( "
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_TYPE + " INT NOT NULL,"
+                    + COLUMN_CONTENT + " TEXT NOT NULL, "
+                    + COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+                    + ");";
+            Log.d("Database Helper", "Executing: " + CREATE_LOG);
+
+            db.execSQL(CREATE_LOG);
+            logNo.close();
+            Log.d("Database Helper", "Done making new log");
+            return null;
+        }
+    }
 }
 
-class asyncOnUpgrade extends AsyncTask<Void, Void, Void> {
-    private SQLiteDatabase db;
-    asyncOnUpgrade(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "ReadLogs task created");
-        db = database;
-    }
 
-    @Override
-    protected Void doInBackground(Void... voids) {
-        // Make sure there's not a old table to interupt this process
-        db.execSQL("DROP TABLE IF EXISTS " + asyncDatabaseHelper.HANDLER_TABLE_NAME + "_OLD;");
-
-        // Rename table
-        db.execSQL("ALTER TABLE " + asyncDatabaseHelper.HANDLER_TABLE_NAME +
-                " RENAME TO " + asyncDatabaseHelper.HANDLER_TABLE_NAME + "_OLD;");
-
-        // Transfer Data from old table
-        // TODO when relevant
-
-        return null;
-    }
-}
-
-class readIndexLastLog extends AsyncTask<Void, Void, Cursor> {
-    private SQLiteDatabase db;
-    readIndexLastLog(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "ReadLogs task created");
-        db = database;
-    }
-
-    @Override
-    protected Cursor doInBackground(Void... voids) {
-        return db.query(asyncDatabaseHelper.HANDLER_TABLE_NAME,
-                new String[]{asyncDatabaseHelper.COLUMN_ID},
-                null, null, null, null,
-                asyncDatabaseHelper.COLUMN_ID + " DESC",
-                "1");
-    }
-}
-
-class asyncReadLog extends AsyncTask<Integer, Void, Cursor> {
-    private SQLiteDatabase db;
-    asyncReadLog(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "ReadLog task created");
-        db = database;
-    }
-
-    @Override
-    protected Cursor doInBackground(Integer... ints) {
-        return db.query(asyncDatabaseHelper.LOG + ints[0],
-                new String[] {asyncDatabaseHelper.COLUMN_TYPE,
-                              asyncDatabaseHelper.COLUMN_CONTENT,
-                              asyncDatabaseHelper.COLUMN_TIMESTAMP},
-                null, null, null, null,
-                asyncDatabaseHelper.COLUMN_ID + " ASC",
-                null);
-    }
-}
-
-class asyncReadLogs extends AsyncTask<Void, Void, Cursor> {
-    private SQLiteDatabase db;
-    asyncReadLogs(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "ReadLogs task created");
-        db = database;
-    }
-
-    @Override
-    protected Cursor doInBackground(Void... voids) {
-        return db.query(asyncDatabaseHelper.HANDLER_TABLE_NAME,
-                new String[]{asyncDatabaseHelper.COLUMN_ID,
-                             asyncDatabaseHelper.LOG,
-                             asyncDatabaseHelper.COLUMN_TIMESTAMP},
-                null, null, null, null,
-                asyncDatabaseHelper.COLUMN_ID + " ASC",
-                null);
-    }
-}
-
-class asyncOnCreate extends AsyncTask<Void, Void, Void> {
-    private SQLiteDatabase db;
-    asyncOnCreate(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "On create task created");
-        db = database;
-    }
-
-    @Override
-    protected Void doInBackground(Void... voids) {
-        String CREATE_HANDLER_TABLE =
-                "CREATE TABLE IF NOT EXISTS " + asyncDatabaseHelper.HANDLER_TABLE_NAME + "("
-                        + asyncDatabaseHelper.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                        + asyncDatabaseHelper.LOG + " TINYTEXT,"
-                        + asyncDatabaseHelper.COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-                        + ");";
-
-        db.execSQL(CREATE_HANDLER_TABLE);
-        return null;
-    }
-}
-
-class asyncDelLog extends AsyncTask<Integer, Void, Void> {
-    private SQLiteDatabase db;
-    asyncDelLog(SQLiteDatabase database)
-    {
-        Log.d("Database Helper", "Delete Log created");
-        db = database;
-    }
-
-    @Override
-    protected Void doInBackground(Integer... ints) {
-        String del = "DROP TABLE IF EXISTS " + asyncDatabaseHelper.LOG + ints[0] + ";";
-        db.execSQL(del);
-        Log.d("Database Helper","Dropped Table Log " + ints[0]);
-
-        db.delete(asyncDatabaseHelper.HANDLER_TABLE_NAME,
-                asyncDatabaseHelper.COLUMN_ID + "=" + ints[0],
-                null);
-        Log.d("Database Helper","Removed Index ID " + ints[0]);
-
-        return null;
-    }
-}
-
-class asyncNewLog extends AsyncTask<Void, Void, Void> {
-    private SQLiteDatabase db;
-    private ContentValues insertValue = new ContentValues(0);
-
-    asyncNewLog(SQLiteDatabase database, String name)
-    {
-        Log.d("Database Helper", "Make new log created");
-        db = database;
-
-        // For the sake of readability would prefer none to be null
-        if (name.equals("")) { name = null; }
-        insertValue.put(asyncDatabaseHelper.LOG, name);
-        Log.d("Database Helper", "Value Inserted to Index");
-    }
-
-    @Override
-    protected Void doInBackground(Void... voids) {
-        Log.d("Database Helper", "Make new log called");
-        db.insert(asyncDatabaseHelper.HANDLER_TABLE_NAME,
-                null,
-                insertValue);
-        Log.d("Database Helper", "Value Inserted to Index");
-
-        // Query to get the new Log ID to work with
-        final Cursor logNo = db.query(asyncDatabaseHelper.HANDLER_TABLE_NAME,
-                new String[]{asyncDatabaseHelper.COLUMN_ID},
-                null, null, null, null,
-                asyncDatabaseHelper.COLUMN_ID + " DESC", "1");
-        logNo.moveToFirst();
-
-        // Make a new log table
-        String CREATE_LOG = "CREATE TABLE IF NOT EXISTS " + asyncDatabaseHelper.LOG + logNo.getString(0)
-                + "( "
-                + asyncDatabaseHelper.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + asyncDatabaseHelper.COLUMN_TYPE + " INT NOT NULL,"
-                + asyncDatabaseHelper.COLUMN_CONTENT + " TEXT NOT NULL, "
-                + asyncDatabaseHelper.COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-                + ");";
-        Log.d("Database Helper", "Executing: " + CREATE_LOG);
-
-        db.execSQL(CREATE_LOG);
-        logNo.close();
-        Log.d("Database Helper", "Done making new log");
-        return null;
-    }
-}
