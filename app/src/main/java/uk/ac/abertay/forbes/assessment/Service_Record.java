d1a81de1bd307fe6aa55e6c69d9fb6ddeb481e92
yeroc_sebrof <1500812@abertay.ac.uk>
2018-04-25 23:45:02 +0100

    Repeats a single update of location rn

--- app/src/main/java/uk/ac/abertay/forbes/assessment/Service_Record.java
+++ app/src/main/java/uk/ac/abertay/forbes/assessment/Service_Record.java
@@ -1,5 +1,6 @@
 package uk.ac.abertay.forbes.assessment;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.IntentService;
 import android.app.Notification;
@@ -8,28 +9,32 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Date;
+import java.util.List;
 
 import uk.ac.abertay.forbes.assessment.Activity_Record.BoolObj; // Should act like a pointer
 
 public class Service_Record extends IntentService {
+    private static final String TAG = "Recording Service";
+
     public AsyncDatabaseHelper dh;
     public SQLiteDatabase db;
 
     Activity_Record recordingScreen;
 
     Integer ttl, gps_wait;
-    public Integer logid;
     Boolean call, sms, gps;
 
     BoolObj power_saving_ptr;
@@ -37,31 +42,42 @@ public class Service_Record extends IntentService {
     final Handler ticker = new Handler();
 
     String gpsProv;
-    IncomingSms smsListener = null;
-    CallReceiver callReceiver = null;
+//    SmsReceiver smsReceiver = null;
+//    CallReceiver callReceiver = null;
+    LocationManager locationManager = null;
 
     Runnable waitCode = new Runnable() {
         @Override
         public void run() {
-            Log.d("Recording Service", "1 Min Tick - TTL is " + ttl.toString());
+            Log.d(TAG, "1 Min Tick - TTL is " + ttl.toString());
 
             ttl--;
 
-            if (gps) {
-                if (power_saving_ptr.value)
-                    // GPS resume method
-                    Log.d("Recording Service", "GPS goes");
-                else
-                    // GPS Pause
-                    Log.d("Recording Service", "GPS stays");
-            }
-
-            if (ttl % gps_wait == 0 && gps)
+            if (gps && ContextCompat.checkSelfPermission(recordingScreen.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == 0)
             {
-                // gps ping
+                if ((power_saving_ptr.value ? ttl * 2 : ttl)
+                        % gps_wait == 0)
+                {
+                    Location meme = locationManager.getLastKnownLocation(gpsProv);
+
+                    if (meme == null)
+                    {
+                        gps = false;
+                        Toast.makeText(recordingScreen, "Unable to fetch GPS location", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Log.d(TAG, "LOCATION EXAMPLE - " + meme.getLatitude() + ", " + meme.getLongitude());
+
+                        dh.newActivity(db, 0, "{\n\t\"lat-float\":" + meme.getLatitude()
+                                + ",\n\t\"long-float\":" + meme.getLongitude() + "\n}");
+                    }
+
+
+                }
             }
 
-            if (ttl >= 0)
+            if (ttl > 0)
                 ticker.postDelayed(this, 5000); // TODO FIND 5000 REPLACE WITH 60000
             else {
                 recordingScreen.recordingComplete();
@@ -70,7 +86,6 @@ public class Service_Record extends IntentService {
     };
 
     // Copied from documentation
-    NotificationManager mNM;
     final IBinder mBinder = new LocalBinder();
 
     public Service_Record(String name) {
@@ -85,7 +100,7 @@ public class Service_Record extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         // Get data from intent
-        Log.d("Recording Service", "Handle Intent called");
+        Log.d(TAG, "Handle Intent called");
         ttl = intent.getExtras().getInt("MinToLive");
 
         gps_wait = intent.getExtras().getInt("gps_time");
@@ -112,10 +127,6 @@ public class Service_Record extends IntentService {
     @Override
     public void onDestroy() {
         Log.d("Record Service", "Stopping Service - Since you asked so nicely");
-
-        // Cancel the persistent notification.
-        mNM.cancel(R.string.app_name);
-
         // stop ticker if its still on the go
         ticker.removeCallbacks(waitCode);
 
@@ -124,22 +135,26 @@ public class Service_Record extends IntentService {
                 .show();
     }
 
-    @SuppressLint("MissingPermission")
     public void setupListeners () {
         // Start listeners
-        Log.d("Recording Service", "Starting Listeners");
+        Log.d(TAG, "Starting Listeners");
         if (gps) {
-            Log.d("Recording Service", "GPS Pinged");
-            LocationManager locationManager = (LocationManager) recordingScreen.getSystemService(LOCATION_SERVICE);
-            Criteria forGPS = new Criteria();
-            forGPS.setAccuracy(Criteria.ACCURACY_FINE);
+            Log.d(TAG, "GPS");
+            locationManager = (LocationManager) recordingScreen.getSystemService(LOCATION_SERVICE);
+            List <String> providers = locationManager.getProviders(true);
 
-            gpsProv = locationManager.getBestProvider(forGPS, true);
+            for (String provider: providers) {
+                if (!provider.equals("passive")) {
+                    gpsProv = provider;
+                    break;
+                }
+            }
 
-            if (gpsProv == null) {
-                Log.d("Recording Service", "LOCATION EXAMPLE - " + locationManager.getLastKnownLocation(gpsProv));
+            if (gpsProv != null && ContextCompat.checkSelfPermission(recordingScreen.getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) == 0) {
+                Log.d(TAG, "LOCATION EXAMPLE - " + locationManager.getLastKnownLocation(gpsProv).getAccuracy() + "m Accuracy");
             } else {
-                Log.d("Recording Service", "No provider for GPS");
+                Log.d(TAG, "No provider for GPS");
                 Toast.makeText(recordingScreen, "GPS is unavailable", Toast.LENGTH_SHORT).show();
 
                 gps = false;
@@ -148,17 +163,40 @@ public class Service_Record extends IntentService {
             }
         }
 
-        if (call) {
-            Log.d("Recording Service", "Starting Calls");
-            callReceiver = new CallReceiver();
-            callReceiver.setReq(this);
-        }
-
-        if (sms) {
-            Log.d("Recording Service", "Starting Texts");
-            smsListener = new IncomingSms();
-            smsListener.setParent(this);
-        }
+        if (call || sms)
+            Toast.makeText(recordingScreen, "Call and Text Recording is currently unavailable", Toast.LENGTH_LONG).show();
+//
+//        if (call) {
+//            Log.d(TAG, "Starting Calls");
+//            callReceiver = new CallReceiver();
+//            callReceiver.setListener(new CallReceiver.Listener() {
+//                @Override
+//                public void onCallReceived(String number, Boolean outbound, Date start) {
+//                    if (start != null)
+//                    {
+//                        dh.newActivity(db, 2,"{\n\t\"contact\":\"" + number + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"" + start.toString() + "\"\n}");
+//                    }
+//                    else
+//                    {
+//                        dh.newActivity(db, 2,"{\n\t\"contact\":\"" + number + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"none\"\n}");
+//                    }
+//                }
+//            });
+//        }
+//
+//        if (sms) {
+//            Log.d(TAG, "Starting Texts");
+//            smsReceiver = new SmsReceiver();
+//            smsReceiver.setListener(new SmsReceiver.Listener() {
+//                @Override
+//                public void onTextReceived(String sender, String text) {
+//                    Log.d("SmsReceiver", "senderNum: " + sender + "; message: " + text);
+//                    dh.newActivity(db, 2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":false\n\t\"content\":\"" + text + "\"\n}");
+//                }
+//            });
+//
+//            registerReceiver(smsReceiver, new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+//        }
     }
 
     @Override
@@ -175,49 +213,30 @@ public class Service_Record extends IntentService {
         ticker.postDelayed(waitCode, 5000);
 
         // Service Run successfully
-        Log.d("Recording Service", "Successful run");
+        Log.d(TAG, "Successful run");
         return START_STICKY;
     }
 
-    // copied from documentation
-    public void showNotification() {
-        // The PendingIntent to launch our activity if the user selects this notification
-        PendingIntent contentIntent = PendingIntent.getActivity(this, R.string.app_name,
-                new Intent(this, getClass()), 0);
-
-        // Set the info for the views that show in the notification panel.
-        Notification notification = new Notification.Builder(this)
-                .setSmallIcon(R.drawable.cpt)  // the status icon
-                .setTicker(getText(R.string.recording_active))  // the status text
-                .setWhen(System.currentTimeMillis())  // the time stamp
-                .setContentTitle(getText(R.string.app_name))  // the label of the entry
-                .setContentText(getText(R.string.recording_active))  // the contents of the entry
-                .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
-                .build();
-
-        // Send the notification.
-        mNM.notify(R.string.app_name, notification);
-    }
-
-    public void gotAText(String sender, String text) {
-        dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":false\n\t\"content\":\"" + text + "\"\n}");
-    }
-
-    public void gotACall(String sender, Boolean outbound,@Nullable Date start) {
-        if (start != null)
-        {
-            dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"" + start.toString() + "\"\n}");
-        }
-        else
-        {
-            dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"none\"\n}");
-        }
-    }
+//    public void gotAText(String sender, String text) {
+//        dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":false\n\t\"content\":\"" + text + "\"\n}");
+//    }
+//
+//    public void gotACall(String sender, Boolean outbound,@Nullable Date start) {
+//        if (start != null)
+//        {
+//            dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"" + start.toString() + "\"\n}");
+//        }
+//        else
+//        {
+//            dh.newActivity(db, logid,2,"{\n\t\"contact\":\"" + sender + "\",\n\t\"outbound\":" + outbound.toString() + "\n\t\"start\":\"none\"\n}");
+//        }
+//    }
 
     public void stopPls() {
         Log.d("Record Service", "Stopping Service - Since you asked so nicely");
         ticker.removeCallbacks(waitCode);
 //        mNM.cancel(R.string.app_name);
+//        if (sms) unregisterReceiver(smsReceiver);
         super.stopSelf();
     }
 }
